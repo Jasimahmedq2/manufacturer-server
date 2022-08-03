702e28b90138f10f93bce5c8e5c4a78eaff75835
jasimahmedq2 <jasimahmed90852417>
2022-08-03 18:19:40 +0600

    deploy-horoku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -12,33 +12,98 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k3ork.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log('uri', uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(process.env.DB_USER, process.env.DB_PASS)
-
 async function run() {
-  try{
-     await client.connect()
-     const groceryCollection = client.db('warehouse').collection('grocery')
-     app.get('/item', async(req, res) => {
-       const query = {};
-       const cursor = groceryCollection.find(query);
-       const items = await cursor.toArray();
-       res.send(items)
-     })
+  try {
+    await client.connect()
+    const groceryCollection = client.db('groceryManagment').collection('product');
+    const addServiceCollection = client.db('groceryManagment').collection('service')
+
+    app.get('/item', async (req, res) => {
+      const query = {};
+      const cursor = groceryCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    })
+
+
+    app.get('/item/:id', async (req, res) => {
+      const id = req.params;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      console.log(query)
+      const service = await groceryCollection.findOne(query);
+      console.log(service)
+      res.send(service);
+    });
+
+    //delete
+
+    app.delete('/item/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await groceryCollection.deleteOne(query);
+      res.send(result);
+    });
+    
+    //  Post
+
+    app.post('/order', async(req, res) => {
+      const data = req.body;
+      const result = await addServiceCollection.insertOne(data)
+      res.send(result)
+    })
+
+    // get 
+
+    app.get('/order', async(req, res) => {
+      const email = req.query.email;
+      console.log(email)
+      const query = {email:email};
+      const cursor = addServiceCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // delete
+
+    app.delete('/order/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)}
+      const result = await addServiceCollection.deleteOne(query);
+      res.send(result)
+    })
+   
+
+    app.put('/item/:id', async (req, res) => {
+      const id = (req.params.id)
+      const updateNum = req.body;
+      console.log(updateNum)
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateProduct = {
+          $set: {
+              quantity: updateNum.quantityNum
+          },
+      };
+
+      const result = await groceryCollection.updateOne(query, updateProduct, options);
+      console.log(result);
+
+      res.send(result);
+
+  });
+
   }
-  finally{
+  finally {
 
   }
 
 }
-
 run().catch(console.dir)
-
-
-
-
 
 app.get('/', (req, res) => {
   res.send('warehouse grocery managment')
